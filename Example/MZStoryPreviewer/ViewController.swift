bd5e2a0a6fbe69b41fca39d2cb14ddd5ab5d5439
mozead1996 <mohamedzead2021@gmail.com>
2020-10-28 22:48:40 +0200

    Initial commit

---
+++ Example/MZStoryPreviewer/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MZStoryPreviewer
+//
+//  Created by mozead1996 on 10/28/2020.
+//  Copyright (c) 2020 mozead1996. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
